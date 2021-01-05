9f9c59e42a3e92a2e8a87a32de4ef8cac94fc574
Andrew Yong <me@ndoo.sg>
2021-01-06 07:34:14 +0800

    Use deep sleep for CALE slides example, retry if drawBitmapFromWeb fails

--- examples/CALE/cale-slides.cpp
+++ examples/CALE/cale-slides.cpp
@@ -4,6 +4,8 @@
    Added deepsleep functionality so it wakes up each N minutes and downloads a new image
 */
 
+#include <rom/rtc.h>                // Include ESP32 library for RTC (needed for rtc_get_reset_reason() function)
+#include "driver/rtc_io.h"          // Include ESP32 library for RTC pin I/O (needed for rtc_gpio_isolate() function)
 #include "Inkplate.h"               //Include Inkplate library to the sketch
 #include "HTTPClient.h"             //Include library for HTTPClient
 #include "WiFi.h"                   //Include library for WiFi
@@ -14,7 +16,7 @@ Inkplate display(INKPLATE_1BIT);    //Create an object on Inkplate library and a
 
 // Einkplate will sleep this minutes after rendering the image. 
 // Please don't make this shorter than once per hour since it's using our shared server!
-#define SLIDES_CHANGE_SECONDS 30
+#define SLIDES_CHANGE_SECONDS 1800
 
 char * screenUrl = SCREEN_URL;
 uint64_t USEC = 1000000;
@@ -22,36 +24,30 @@ uint64_t USEC = 1000000;
 void setup() {
   Serial.begin(115200);
   display.begin();        //Init Inkplate library (you should call this function ONLY ONCE)
-  display.clearDisplay(); //Clear frame buffer of display
-  display.display();      //Put clear image on display
-  display.setTextColor(BLACK, WHITE); // Set text color to be black and background color to be white
-  display.setTextSize(4);             // Set text to be 4 times bigger than classic 5x7 px text
-
-  display.print("Connecting to WiFi...");
-  display.partialUpdate(); // Does not print the partial update like before
 
   //Connect to the WiFi network.
+  Serial.println("Connecting to WiFi");
   WiFi.mode(WIFI_MODE_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    display.print(".");
-    display.partialUpdate();
+  while (WiFi.status() != WL_CONNECTED) delay(0);
+
+  uint8_t attempts = 3;
+  while (attempts--) {
+    if (display.drawBitmapFromWeb(screenUrl, 0, 0, false)) {
+      WiFi.mode(WIFI_OFF); // Reduce power
+      display.display();
+      break;
+    } else {
+      Serial.println("Image open error");
+    }
   }
-  display.println("\nWiFi OK! Downloading...");
-  display.partialUpdate(); 
+
+  Serial.println("Going to sleep...");
+  esp_sleep_enable_timer_wakeup(SLIDES_CHANGE_SECONDS * USEC);  // Set EPS32 to be woken up in SLIDES_CHANGE_SECONDS seconds (in this case)
+  rtc_gpio_isolate(GPIO_NUM_12);                                          // Isolate/disable GPIO12 on ESP32 (only to reduce power consumption in sleep)
+  esp_deep_sleep_start();
 }
 
 void loop() {
-  //Draw image from CALE.es
-  Serial.printf("Rendering screen again from: %s\n",screenUrl);
-  //display.fillScreen(WHITE);
-
-  if (!display.drawBitmapFromWeb(screenUrl, 0, 0, false)) {
-    //If is something failed (wrong filename or wrong bitmap format), write error message on the screen.
-    //REMEMBER! You can only use Windows Bitmap file with color depth of 1, 4, 8 or 24 bits with no compression! 
-    display.println("Image open error");
-  }
-  display.display();
-  delay(SLIDES_CHANGE_SECONDS*1000);
+  // Nothing should be here while using deep sleep!
 }
